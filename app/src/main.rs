5024396ee0d181000145305ece72447751f22d1c
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2024-01-28 06:14:29 -0700

    Submitted initial steps

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,23 +1,60 @@
-use fantoccini::{ClientBuilder, Locator};
+use fantoccini::{elements::Element, wd::Capabilities, ClientBuilder, Locator};
+
+// async fn available_rooms(
+//     c: &fantoccini::Client,
+//     date:
+// )
 
 // let's set up the sequence of steps we want the browser to take
 #[tokio::main]
 async fn main() -> Result<(), fantoccini::error::CmdError> {
-    let c = ClientBuilder::native().connect("http://localhost:4444").await.expect("failed to connect to WebDriver");
+    // https://github.com/jonhoo/fantoccini/issues/45#issuecomment-1546600219
+    let cap: Capabilities = serde_json::from_str(
+        r#"{"browserName":"chrome","goog:chromeOptions":{"args":["--headless"]}}"#,
+    )
+    .unwrap();
+    let c = ClientBuilder::native()
+        .capabilities(cap)
+        .connect("http://localhost:9515")
+        .await
+        .expect("failed to connect to WebDriver");
 
-    // first, go to the Wikipedia page for Foobar
-    c.goto("https://en.wikipedia.org/wiki/Foobar").await?;
+    c.set_window_rect(0, 0, 1920, 1080).await?;
+
+    c.goto("https://calgarylibrary.ca/events-and-programs/book-a-space/book-a-room/")
+        .await?;
     let url = c.current_url().await?;
-    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foobar");
+    assert_eq!(
+        url.as_ref(),
+        "https://calgarylibrary.ca/events-and-programs/book-a-space/book-a-room/"
+    );
 
-    // click "Foo (disambiguation)"
-    c.find(Locator::Css(".mw-disambig")).await?.click().await?;
+    let screenshot_png = c.screenshot().await.unwrap();
+    std::fs::write("0.png", screenshot_png).unwrap();
 
-    // click "Foo Lake"
-    c.find(Locator::LinkText("Foo Lake")).await?.click().await?;
+    // There are more than one button that matches the selector
+    let buttons = c
+        .find_all(Locator::Css("button.btn-submission.red[value='Search']"))
+        .await?;
+    let mut button: Option<Element> = None;
+    for b in buttons {
+        if b.is_displayed().await? {
+            button = match button {
+                None => Some(b),
+                Some(_) => panic!("More than one button found"),
+            };
+            break;
+        }
+    }
+    let button = button.unwrap();
+    let screenshot_png = button.screenshot().await.unwrap();
+    std::fs::write("1.png", screenshot_png).unwrap();
 
-    let url = c.current_url().await?;
-    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foo_Lake");
+    // // click "Foo Lake"
+    // c.find(Locator::LinkText("Foo Lake")).await?.click().await?;
+
+    // let url = c.current_url().await?;
+    // assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foo_Lake");
 
     c.close().await
 }

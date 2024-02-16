556094fcf32bf32a521282c4ea21c9f7155161f7
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2024-02-15 22:27:48 -0700

    Started rerunning the build.rs only in case of contents changes in package.json

--- wry_app/build.rs
+++ wry_app/build.rs
@@ -1,3 +1,6 @@
+use std::io::{Read, Write};
+
+use sha2::{Digest, Sha256};
 use which::which;
 
 const TRACKED_FRONTEND_PATHS: &[&str] = &[
@@ -11,10 +14,7 @@ const TRACKED_FRONTEND_PATHS: &[&str] = &[
     "src",
 ];
 
-pub fn main() {
-    for p in TRACKED_FRONTEND_PATHS.iter() {
-        println!("cargo:rerun-if-changed=../front/{}", p);
-    }
+fn build_frontend() {
     let npm = which("npm").expect("npm not found");
 
     std::process::Command::new(&npm)
@@ -27,4 +27,85 @@ pub fn main() {
         .current_dir("../front")
         .output()
         .unwrap();
+}
+
+fn compute_package_lock_hash() -> [u8; 32] {
+    let package_lock = std::fs::File::open("../front/package-lock.json")
+        .expect("../front/package-lock.json not found");
+
+    let mut package_lock_reader = std::io::BufReader::new(package_lock);
+
+    let mut hasher = Sha256::new();
+    let mut buffer = [0; 1024];
+    loop {
+        let count = package_lock_reader.read(&mut buffer).unwrap();
+        if count == 0 {
+            break;
+        }
+        hasher.update(&buffer[..count]);
+    }
+    hasher.finalize().into()
+}
+
+fn write_package_lock_hash(hash: [u8; 32]) {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let hash_path = std::path::Path::new(&out_dir).join("package-lock-hash");
+
+    let mut hash_file = std::fs::File::create(hash_path).unwrap();
+    hash_file.write_all(&hash).unwrap();
+}
+
+mod compare_and_set_package_lock_hash {
+    pub(crate) enum Outcome {
+        Equal,
+        Updated,
+        FreshlyWritten,
+    }
+
+    impl Outcome {
+        pub(crate) fn has_changed(&self) -> bool {
+            !matches!(self, Self::Equal)
+        }
+    }
+}
+
+// Returns true if equal. Returns false if not equal or file does not exist.
+fn compare_and_set_package_lock_hash(
+    computed: [u8; 32],
+) -> compare_and_set_package_lock_hash::Outcome {
+    use compare_and_set_package_lock_hash::Outcome::{Equal, FreshlyWritten, Updated};
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let hash_path = std::path::Path::new(&out_dir).join("package-lock-hash");
+
+    if !hash_path.exists() {
+        write_package_lock_hash(computed);
+        return FreshlyWritten;
+    }
+
+    let mut hash_file = std::fs::File::open(hash_path).unwrap();
+    let mut stored_hash = [0; 32];
+    hash_file.read_exact(&mut stored_hash).unwrap();
+    if stored_hash == computed {
+        Equal
+    } else {
+        write_package_lock_hash(computed);
+        Updated
+    }
+}
+
+pub fn main() {
+    for p in TRACKED_FRONTEND_PATHS.iter() {
+        println!("cargo:rerun-if-changed=../front/{}", p);
+    }
+
+    if !std::path::Path::new("../front/node_modules").exists() {
+        build_frontend();
+        return;
+    };
+
+    let computed_package_lock_hash = compute_package_lock_hash();
+    if compare_and_set_package_lock_hash(computed_package_lock_hash).has_changed() {
+        build_frontend();
+    }
 }
